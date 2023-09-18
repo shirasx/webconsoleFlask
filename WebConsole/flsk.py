ec02e1b50860ff099eeb8789994e4e965120f7f8
anata wa dare <122711247+shirasx@users.noreply.github.com>
2023-09-18 09:33:56 -0300

    Add files via upload
    Added directory change

--- WebConsole/flsk.py
+++ WebConsole/flsk.py
@@ -1,26 +1,38 @@
 from flask import Flask, render_template, request
-import requests
 import subprocess
+import os
 
 app = Flask(__name__)
 
+
+diretorio_atual = os.getcwd()
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", diretorio_atual=diretorio_atual)
 
 @app.route("/executar_comando", methods=["POST"])
 def executar_comando():
+    global diretorio_atual
     comando = request.form.get("comando")
-    
-    # Executar o comando usando subprocess
-    try:
-        resultado = subprocess.check_output(comando, shell=True, stderr=subprocess.STDOUT, text=True)
-    except subprocess.CalledProcessError as e:
-        resultado = str(e.output)
-    
-    # Renderize o mesmo template com o resultado
-    return render_template("index.html", resultado=resultado)
 
+    if comando.startswith("cd "):
+        novo_diretorio = comando[3:].strip()  
+        try:
+            os.chdir(novo_diretorio)
+            diretorio_atual = os.getcwd()
+            resultado = f"Diretório alterado para {diretorio_atual}"
+        except Exception as e:
+            resultado = f"Erro ao acessar o diretório: {str(e)}"
+    else:
+        try:
+            resultado = subprocess.check_output(comando, shell=True, stderr=subprocess.STDOUT, text=True)
+        except subprocess.CalledProcessError as e:
+            resultado = str(e.output)
+        except Exception as e:
+            resultado = str(e)
+
+    return render_template("index.html", resultado=resultado, diretorio_atual=diretorio_atual)
 
 if __name__ == "__main__":
     app.run(port=8080)
